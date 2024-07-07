68419d1cb022d81e5299f8cd6d8d1e803d78033a
ivnikonHA <ivnikon@mail.ru>
2024-07-07 22:30:51 +0300

    Добавляет микросервисы posts и notifications. Переименовывает user в users

--- project/apps/users/jest.config.ts
+++ project/apps/users/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'user',
+  displayName: 'users',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/user'
+  coverageDirectory: '../../coverage/apps/users',
 };
