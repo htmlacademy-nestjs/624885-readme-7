93b2ed1827c503bdc5af95769c8804228c2a6df1
ivnikonHA <ivnikon@mail.ru>
2024-07-11 22:08:46 +0300

    Добавит обработчик маршрута /register

--- project/apps/users/src/app/authentication/authentication.service.ts
+++ project/apps/users/src/app/authentication/authentication.service.ts
@@ -12,7 +12,7 @@ export class AuthenticationService {
     private readonly blogUserRepository: BlogUserRepository
   ) {}
 
-  public async register(dto: CreateUserDto) {
+  public async register(dto: CreateUserDto): Promise<BlogUserEntity> {
     const { email, name, password } = dto;
 
     const blogUser: AuthUser = {
@@ -28,8 +28,9 @@ export class AuthenticationService {
     }
 
     const userEntity = await new BlogUserEntity(blogUser).setPassword(password);
+    this.blogUserRepository.save(userEntity);
 
-    return this.blogUserRepository.save(userEntity);
+    return userEntity;
   }
 
   public async verifyUser(dto: LoginUserDto) {
