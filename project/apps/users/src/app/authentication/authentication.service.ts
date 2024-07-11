8a3f25e079f6ca4d2e0ea6d492cbb80f788ff0e3
ivnikonHA <ivnikon@mail.ru>
2024-07-11 21:50:50 +0300

    Добавит метод регистрации в AuthenticationService

--- project/apps/users/src/app/authentication/authentication.service.ts
+++ project/apps/users/src/app/authentication/authentication.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@nestjs/common';
-import { BlogUserRepository } from '@users/blog-user'
+import { ConflictException, Injectable } from '@nestjs/common';
+
+import { BlogUserEntity, BlogUserRepository } from '@users/blog-user'
+import { CreateUserDto } from './dto/create-user.dto';
+import { AuthUser } from '@project/core';
+import { AUTH_USER_EXISTS } from './authentication.constant';
 
 @Injectable()
 export class AuthenticationService {
   constructor(
     private readonly blogUserRepository: BlogUserRepository
   ) {}
+
+  public async register(dto: CreateUserDto) {
+    const { email, name, password } = dto;
+
+    const blogUser: AuthUser = {
+      email,
+      name,
+      avatar: '',
+      passwordHash: ''
+    }
+
+    const existUser = await this.blogUserRepository.findByEmail(email);
+    if(existUser) {
+      throw new ConflictException(AUTH_USER_EXISTS);
+    }
+
+    const userEntity = await new BlogUserEntity(blogUser).setPassword(password);
+
+    return this.blogUserRepository.save(userEntity);
+  }
 }
