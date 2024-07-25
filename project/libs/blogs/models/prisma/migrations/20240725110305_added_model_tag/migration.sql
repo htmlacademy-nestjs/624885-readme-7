653d2b7b7d2ea1158d609d91245562ead30b476e
ivnikonHA <ivnikon@mail.ru>
2024-07-25 14:05:46 +0300

    Добавляет модель Tag

---
+++ project/libs/blogs/models/prisma/migrations/20240725110305_added_model_tag/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "tags" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "tags_pkey" PRIMARY KEY ("id")
+);
