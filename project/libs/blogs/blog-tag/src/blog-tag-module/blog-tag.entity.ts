ff734140b542d8ad032da8b82f961d36edda43d6
ivnikonHA <ivnikon@mail.ru>
2024-08-05 13:56:47 +0300

    Добавляет BlogTagEntity

---
+++ project/libs/blogs/blog-tag/src/blog-tag-module/blog-tag.entity.ts
@@ -0,0 +1,32 @@
+import { Entity, StorableEntity, Tag } from '@project/core';
+
+export class BlogTagEntity extends Entity implements StorableEntity<Tag> {
+  public title: string;
+  public createdAt: Date;
+  public updatedAt: Date;
+
+  constructor(tag?: Tag) {
+    super();
+    this.populate(tag);
+  }
+
+  public populate(tag?: Tag) {
+    if(!tag) {
+      return;
+    }
+
+    this.id = tag.id ?? undefined;
+    this.title = tag.title;
+    this.createdAt = tag.createdAt ?? undefined;
+    this.updatedAt = tag.updatedAt ?? undefined;
+  }
+
+  public toPOJO(): Tag {
+    return {
+      id: this.id,
+      title: this.title,
+      createdAt: this.createdAt,
+      updatedAt: this.updatedAt
+    }
+  }
+}
--- project/libs/shared/core/src/lib/types/tag.interface.ts
+++ project/libs/shared/core/src/lib/types/tag.interface.ts
@@ -1,9 +1,6 @@
-import { Post } from '@project/core';
-
 export interface Tag {
   id?: string;
   title: string;
-  posts: Post[];
   createdAt?: Date;
   updatedAt?: Date;
 }
