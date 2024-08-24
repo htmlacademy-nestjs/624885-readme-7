7de29eca77465e4a757df913b380088d345bba4a
ivnikonHA <ivnikon@mail.ru>
2024-08-24 17:21:17 +0300

    Подготовит инфраструктуру для сохранения данных о файле в БД

--- project/apps/files/src/app/app.module.ts
+++ project/apps/files/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { FileUploaderModule } from '@project/file-uploader';
-import { FilesConfigModule } from '@project/files-config';
+import { FilesConfigModule, getMongooseOptions } from '@project/files-config';
 
 @Module({
   imports: [
     FileUploaderModule,
-    FilesConfigModule
+    FilesConfigModule,
+    MongooseModule.forRootAsync(getMongooseOptions())
   ],
   controllers: [],
   providers: [],
