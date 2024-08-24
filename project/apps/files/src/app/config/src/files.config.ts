32884b228a1f0c99ef264e7ae9f5acf5b7258852
ivnikonHA <ivnikon@mail.ru>
2024-08-24 16:50:24 +0300

    Добавляет загрузку файла по статическому маршруту

--- project/apps/files/src/app/config/src/files.config.ts
+++ project/apps/files/src/app/config/src/files.config.ts
@@ -3,6 +3,7 @@ import * as Joi from 'joi';
 
 const DEFAULT_PORT = 3000;
 const ENVIRONMENTS = ['development', 'production', 'stage'] as const;
+const DEFAULT_SERVE_ROOT = '/';
 
 type Environment = typeof ENVIRONMENTS[number];
 
@@ -10,12 +11,14 @@ export interface FilesConfig {
   environment: string;
   port: number;
   uploadDirectory: string;
+  serveRoot: string;
 }
 
 const validationSchema = Joi.object({
   environment: Joi.string().valid(...ENVIRONMENTS).required(),
   port: Joi.number().port().default(DEFAULT_PORT),
-  uploadDirectory: Joi.string().required()
+  uploadDirectory: Joi.string().required(),
+  serveRoot: Joi.string().default(DEFAULT_SERVE_ROOT)
 });
 
 function validateConfig(config: FilesConfig): void {
@@ -29,7 +32,8 @@ function getConfig(): FilesConfig {
   const config: FilesConfig = {
     environment: process.env.NODE_ENV as Environment,
     port: parseInt(process.env.PORT || `${DEFAULT_PORT}`, 10),
-    uploadDirectory: process.env.UPLOAD_DIRECTORY
+    uploadDirectory: process.env.UPLOAD_DIRECTORY,
+    serveRoot: process.env.SERVE_ROOT
   };
 
   validateConfig(config);
--- project/apps/files/src/app/file-uploader/src/file-uploader-module/file-uploader.module.ts
+++ project/apps/files/src/app/file-uploader/src/file-uploader-module/file-uploader.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
 import { FileUploaderService } from './file-uploader.service';
 import { FileUploaderController } from './file-uploader.controller';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const rootPath = configService.get<string>('application.uploadDirectory');
+        const serveRoot = configService.get<string>('application.serveRoot')
+        return [{
+          rootPath,
+          serveRoot,
+          serveStaticOptions: {
+            fallthrough: true,
+            etag: true
+          }
+        }]
+      }
+    })
+  ],
   providers: [FileUploaderService],
   controllers: [FileUploaderController]
 })
