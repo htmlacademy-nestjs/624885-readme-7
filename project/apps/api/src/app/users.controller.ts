b29dd0b3d732fd6f6f019dc50efaf3193c56b783
ivnikonHA <ivnikon@mail.ru>
2024-08-26 19:09:47 +0300

    Добавляет AxiosExceptionFilter

--- project/apps/api/src/app/users.controller.ts
+++ project/apps/api/src/app/users.controller.ts
@@ -1,10 +1,12 @@
 import { HttpService } from '@nestjs/axios';
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseFilters } from '@nestjs/common';
 
 import { LoginUserDto } from '@users/authentication';
 import { ApplicationServiceURL } from './app.config';
+import { AxiosExceptionFilter } from './filters/axios-exception.filter';
 
 @Controller('users')
+@UseFilters(AxiosExceptionFilter)
 export class UsersController {
   constructor(
     private readonly httpService: HttpService
